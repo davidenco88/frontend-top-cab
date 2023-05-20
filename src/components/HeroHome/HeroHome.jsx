e69eb272e8bcfc58bf82ff697c2c18a1b5495c8e
David Sarria <david.sarriav@gmail.com>
2023-05-20 14:59:55 +0200

    Feat: Add some modification to Hero

--- src/components/HeroHome/HeroHome.jsx
+++ src/components/HeroHome/HeroHome.jsx
@@ -3,14 +3,22 @@ import './HeroHomeStyles.css';
 function HeroHome() {
   return (
     <div className="hero">
-      <h1>Book Your CAB</h1>
-      <p>Rent Cab With Affordable Price With Rica Cab</p>
-      <form>
-        <input type="radio" name="diff-location" id="diff-location" value="Different" />
-        <label htmlFor="diff-location">Different Location</label>
-        <input type="radio" name="same-location" id="same-location" value="Same" />
-        <label htmlFor="same-location">Same Location</label>
-      </form>
+
+      <div className="hero__content">
+        <h1>Book Your CAB</h1>
+        <p>Rent Cab With Affordable Price With Rica Cab</p>
+        <form>
+          <label htmlFor="diff-location">
+            <input type="radio" name="location" id="diff-location" value="Different" />
+            Different Location
+          </label>
+          <label htmlFor="same-location">
+            <input type="radio" name="location" id="same-location" value="Same" />
+            Same Location
+          </label>
+        </form>
+      </div>
+      <div className="hero__background" />
     </div>
   );
 }
