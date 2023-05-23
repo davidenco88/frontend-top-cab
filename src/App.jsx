5461ee3291bf48cf752d0a791710e869fc45fead
Andres Lopez <73659007+maitahlcor@users.noreply.github.com>
2023-05-23 12:30:45 -0500

    Why use, use steps, download

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 
 import './App.css';
-//import HowToGetCard from'./components/StepsTo/stepsOfUse.jsx';
-import HowToGet from  './components/HowToGet/HowToGet.jsx';
-import WhyUse from './components/WhyUse/WhyUse.jsx'
 function App() {
   return <>
-  <HowToGet/>
   </>;
 }
 
