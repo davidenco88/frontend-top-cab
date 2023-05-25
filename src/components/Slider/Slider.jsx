82df72f9ddbc708a9061211a4efe180c186d849c
Federico <federicoc03@gmail.com>
2023-05-24 23:48:03 -0500

    fix: Slider works

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,31 +1,88 @@
+import { useEffect } from 'react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
+import { GiCarSeat } from 'react-icons/gi';
+import { MdOutlineLuggage } from 'react-icons/md';
+import { IoSnow } from 'react-icons/io5';
+import { FiSettings } from 'react-icons/fi';
 import './Slider.scss';
 import '@splidejs/react-splide/css';
 
 function Slider({ data }) {
+  const getWindowWidth = () => window.innerWidth;
+  const large = {
+    width: '85%',
+    type: 'loop',
+    speed: 1500,
+    perPage: 3,
+    center: true,
+    gap: '50px',
+    snap: true,
+    padding: { left: 60, right: 60 },
+    focus: 'center',
+    autoHeight: true,
+    pagination: false,
+  };
+
+  const medium = {
+    width: '85%',
+    type: 'loop',
+    speed: 1500,
+    perPage: 2,
+    center: true,
+    gap: '50px',
+    snap: true,
+    padding: { left: 60, right: 60 },
+    focus: 'center',
+    autoHeight: true,
+    pagination: false,
+  };
+
+  const small = {
+    width: '85%',
+    type: 'loop',
+    speed: 1500,
+    perPage: 1,
+    center: true,
+    gap: '50px',
+    snap: true,
+    padding: { left: 30, right: 30, bottom: 30 },
+    focus: 'center',
+    autoHeight: true,
+    pagination: false,
+  };
+
+  // useEffect(() => {},[])
+  const getOptions = () => {
+    const width = getWindowWidth();
+
+    if (width > 960) {
+      return large;
+    }
+
+    if (width < 960 && width > 768) {
+      return medium;
+    }
+    if (width < 768) {
+      return small;
+    }
+  };
+
+  const options = getOptions();
+
   return (
     <div className="component">
       <div className="component__info">
         <h3 className="component__info__emphasis">WIDEST VARITY</h3>
         <h2 className="component__info__title">Our Awesome fleet</h2>
         <p className="component__info__description">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis eum
-          voluptas quod, beatae eligendi exercitationem! At quidem alias earum!
-          Perferendis corporis eaque iusto? Vel facere, ipsa temporibus aliquid
-          deleniti et?
+          Choose from a diverse range of meticulously maintained vehicles,
+          equipped with cutting-edge technology for a remarkable ride. Our
+          professional drivers ensure convenience and reliability as you reach
+          your destination effortlessly.
         </p>
       </div>
       <Splide
-        options={{
-          width: '100%',
-          type: 'loop',
-          center: true,
-          // gap: "10px",
-          snap: true,
-          pagination: false,
-          margin: '0',
-          // focus: "center",
-        }}
+        options={options}
       >
         {data.map((item) => (
           <SplideSlide key={item.id}>
@@ -34,7 +91,9 @@ function Slider({ data }) {
               <h2 className="container__textType">{item.service}</h2>
               <h3 className="container__textFee">Fee Start $12 / per Km</h3>
               <p className="container__description">{item.description}</p>
-              <p>S O M E I C O N S</p>
+              <p>
+                <GiCarSeat /> <IoSnow /> <MdOutlineLuggage /> <FiSettings />{' '}
+              </p>
             </div>
           </SplideSlide>
         ))}
