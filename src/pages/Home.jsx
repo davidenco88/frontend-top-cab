7bf2332e07f6acfcad675011e691a9c042b03a4d
Federico <federicoc03@gmail.com>
2023-05-25 20:19:05 -0500

    fix: cleaned Home of unnecessary components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import HeroHome from '../components/HeroHome/HeroHome';
 import FleetSlider from '../components/FleetSlider/FleetSlider';
-import StepsToUse from '../components/StepsToUse/StepsToUse';
-import PaymentOptions from '../components/PaymentOptions/PaymentOptions';
 import data from '../assets/mockdata.json';
 
 function Home() {
@@ -9,10 +7,8 @@ function Home() {
     <div className="page">
       <HeroHome />
       <h1>WELCOME TO RICA</h1>
-      <StepsToUse />
       <h1>Super easy booking</h1>
       <FleetSlider data={data} />
-      <PaymentOptions />
     </div>
   );
 }
