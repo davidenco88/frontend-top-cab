c3d60c378861760c5779df7a76942b2562198938
Andres Lopez <73659007+maitahlcor@users.noreply.github.com>
2023-05-26 13:18:48 -0500

    Hotfix/home (#21)
    * footer to the end
    
    * Revert "footer to the end"
    
    This reverts commit 4c2fae576f807f67cab8f0941d9f1308a2ae2971.
    
    * footer to the end

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,10 @@ function App() {
         <Route path="/cab" element={<Cab />} />
         <Route path="/booking" element={<Booking />} />
       </Routes>
-      <Footer />
       <StepsToUse/>
       <WhyUse/>
       <HowToGet/>
+      <Footer />
     </>
   );
 }
