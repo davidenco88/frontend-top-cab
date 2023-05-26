243ac4923ddefdcd6574165d81b4c3caa918edfc
Federico <federicoc03@gmail.com>
2023-05-26 07:30:46 -0500

    feat: Working on the component

---
+++ src/components/HeroDefault/HeroDefault.jsx
@@ -0,0 +1,14 @@
+import './HeroDefault.scss';
+
+function HeroDefault() {
+  return (
+    <div className="heroContainer">
+      <div className="heroContainer__text">
+        <h2 className="heroContainer__text__title">CAB PAYMENT</h2>
+        <h3 className="container__text__whatever">Home &gt;&gt; Cab Payment</h3>
+      </div>
+    </div>
+  );
+}
+
+export default HeroDefault;
--- src/pages/Cab.jsx
+++ src/pages/Cab.jsx
@@ -1,8 +1,10 @@
 import ListedCar from '../components/ListedCar/ListedCar/ListedCar';
+import HeroDefault from '../components/HeroDefault/HeroDefault';
 
 function Cab() {
   return (
     <>
+      <HeroDefault />
       <h1>Cab</h1>
       <h1>jashgdjshag</h1>
       <ListedCar />
